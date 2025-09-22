0edd50122c74b00483629949cb4b1de1a1468290
Krishna Biswal <krshnabiswal619@gmail.com>
2025-09-22 19:42:00 +0530

    added copy feature and custom TextContent Component

--- src/app/text/page.tsx
+++ src/app/text/page.tsx
@@ -1,31 +1,9 @@
 import React from "react";
-import { Geist_Mono } from "next/font/google";
-import { Copy } from "lucide-react";
-
-const geistMono = Geist_Mono({
-  weight: ["400", "600"],
-  subsets: ["latin"],
-});
-
-function TextPage() {
-  return (
-    <div>
-      <div>
-        <h3 className="text-xl font-semibold">Heading</h3>
-        <hr className="mt-1 mb-3 text-[#f5f5f51f]" />
-        <div className="bg-[#1F2121] rounded-md">
-          <div className="flex justify-between pb-0">
-            <p
-              className={`${geistMono.className} bg-[#2D2F2F] p-1 rounded-tl-md rounded-br-md font-semibold text-xs text-[#949592]`}
-            >
-              text
-            </p>
-            <Copy className="h-4 m-1 cursor-pointer text-[#949592]" />
-          </div>
-          <pre
-            className={`${geistMono.className} p-4 whitespace-pre-wrap text-xs text-[#C5C8C6]`}
-          >
-            {`export default async function getNowPlayingItem() {
+import TextContent from "@/components/textContent";
+const data = [
+  {
+    heading: "Spotify Code",
+    text: `export default async function getNowPlayingItem() {
   const response = await getNowPlaying();
   if (response.status === 204 || response.status > 400) {
     return false;
@@ -41,26 +19,11 @@ function TextPage() {
     isPlaying,
     title,
   };
-}`}
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="text-xl font-semibold">Heading</h3>
-        <hr className="mt-1 mb-3 text-[#f5f5f51f]" />
-        <div className="bg-[#1F2121] rounded-md">
-          <div className="flex justify-between pb-0">
-            <p
-              className={`${geistMono.className} bg-[#2D2F2F] p-1 rounded-tl-md rounded-br-md font-semibold text-xs text-[#949592]`}
-            >
-              text
-            </p>
-            <Copy className="h-4 m-1 cursor-pointer text-[#949592]" />
-          </div>
-          <pre
-            className={`${geistMono.className} p-4 whitespace-pre-wrap text-xs text-[#C5C8C6]`}
-          >
-            {`If the user query is a simple greeting like "hi", "hello", "hey", or "good morning", respond politely with a greeting followed by a reminder: 
+}`,
+  },
+  {
+    heading: "Sample Prompt",
+    text: `If the user query is a simple greeting like "hi", "hello", "hey", or "good morning", respond politely with a greeting followed by a reminder: 
   "Hello! Please ask BSR-related queries."
 
 For other off-topic inputs or non-BSR queries (e.g., "how are you", "what's up"), respond with:
@@ -70,27 +33,11 @@ If the knowledge base result contains relevant information:
    - Respond with TL;DR, detailed explanation, insights, and tables if applicable.
 
 If the knowledge base result is empty or irrelevant:
-   - Respond as mentioned for off-topic inputs above.
-`}
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="text-xl font-semibold">Heading</h3>
-        <hr className="mt-1 mb-3 text-[#f5f5f51f]" />
-        <div className="bg-[#1F2121] rounded-md">
-          <div className="flex justify-between pb-0">
-            <p
-              className={`${geistMono.className} bg-[#2D2F2F] p-1 rounded-tl-md rounded-br-md font-semibold text-xs text-[#949592]`}
-            >
-              text
-            </p>
-            <Copy className="h-4 m-1 cursor-pointer text-[#949592]" />
-          </div>
-          <pre
-            className={`${geistMono.className} p-4 whitespace-pre-wrap text-xs text-[#C5C8C6]`}
-          >
-            {`You are an AI assistant that only answers questions using the provided knowledge base search results.
+   - Respond as mentioned for off-topic inputs above.`,
+  },
+  {
+    heading: "Sample Prompt II",
+    text: `You are an AI assistant that only answers questions using the provided knowledge base search results.
 
 Instructions:
 - Always receive both the user’s query and the knowledge base search result.
@@ -102,46 +49,16 @@ Instructions:
    - Do NOT invent or pull external information.
    - Respond only with a polite message like:
      "Please ask BSR-related queries."
-- Never hallucinate, never search outside the knowledge base, and always stay within these instructions.
-`}
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="text-xl font-semibold">Heading</h3>
-        <hr className="mt-1 mb-3 text-[#f5f5f51f]" />
-        <div className="bg-[#1F2121] rounded-md">
-          <div className="flex justify-between pb-0">
-            <p
-              className={`${geistMono.className} bg-[#2D2F2F] p-1 rounded-tl-md rounded-br-md font-semibold text-xs text-[#949592]`}
-            >
-              text
-            </p>
-            <Copy className="h-4 m-1 cursor-pointer text-[#949592]" />
-          </div>
-          <pre
-            className={`${geistMono.className} p-4 whitespace-pre-wrap text-xs text-[#C5C8C6]`}
-          >
-            {`export default async function getNowPlayingItem() {
-  const response = await getNowPlaying();
-  if (response.status === 204 || response.status > 400) {
-    return false;
-  }
- 
-  const song = await response.json();
-  const artist = song.item.artists[0].name;
-  const isPlaying = song.is_playing;
-  const title = song.item.name;
- 
-  return {
-    artist,
-    isPlaying,
-    title,
-  };
-}`}
-          </pre>
-        </div>
-      </div>
+- Never hallucinate, never search outside the knowledge base, and always stay within these instructions.`,
+  },
+];
+
+function TextPage() {
+  return (
+    <div className="pb-8">
+      {data.map((item, index) => (
+        <TextContent key={index} heading={item.heading} text={item.text} />
+      ))}
     </div>
   );
 }
--- src/components/textContent.tsx
+++ src/components/textContent.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
 
-function TextContent() {
-  return <div>TextContent</div>;
+import React from "react";
+import { Geist_Mono } from "next/font/google";
+import { Copy } from "lucide-react";
+
+const geistMono = Geist_Mono({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+});
+
+type TextContentPropsType = {
+  heading: string;
+  text: string;
+};
+
+function TextContent({ heading, text }: TextContentPropsType) {
+  const onClickCopy = () => {
+    navigator.clipboard.writeText(text);
+  };
+
+  return (
+    <div className="pb-5">
+      <h3 className="text-xl font-semibold">{heading}</h3>
+      <hr className="mt-1 mb-3 text-[#f5f5f51f]" />
+      <div className="bg-[#1F2121] rounded-md">
+        <div className="flex justify-between pb-0">
+          <p
+            className={`${geistMono.className} bg-[#2D2F2F] p-1 rounded-tl-md rounded-br-md font-semibold text-xs text-[#949592]`}
+          >
+            text
+          </p>
+          <Copy
+            className="h-4 m-1 cursor-pointer text-[#949592]"
+            onClick={onClickCopy}
+          />
+        </div>
+        <pre
+          className={`${geistMono.className} p-4 whitespace-pre-wrap text-xs text-[#C5C8C6]`}
+        >
+          {`${text}`}
+        </pre>
+      </div>
+    </div>
+  );
 }
 
 export default TextContent;
