bd1cefc570974f9718da53e77ca14fdbc43da8c0
Krishna Biswal <krshnabiswal619@gmail.com>
2025-09-24 08:06:57 +0530

    added scanQR feature and delete icon

--- src/app/text/layout.tsx
+++ src/app/text/layout.tsx
@@ -16,14 +16,14 @@ function TextLayout({ children }: { children: React.ReactNode }) {
         <p>John Doe</p>
         <CircleUser />
       </nav>
-      <div className="sticky shadow-lg shadow-black top-15 left-0 w-full border-2 rounded-xl mb-5 h-24 bg-[#1F2121] flex flex-col text-xs p-1">
+      <div className="sticky shadow-lg z-1 shadow-black top-15 left-0 w-full border-2 rounded-xl mb-5 h-24 bg-[#1F2121] flex flex-col text-xs p-1">
         <textarea
           placeholder="Paste your text here..."
           className=" border-none outline-none m-2 h-10 text-sm resize-none"
         />
         <div className="flex gap-3 justify-end pr-3 ">
-          <Search className="w-7 cursor-pointer hover:bg-[#2D2F2F] rounded-md p-1" />
-          <Plus className="w-8 cursor-pointer hover:bg-[#2D2F2F] rounded-md p-1" />
+          <Search className="w-8 cursor-pointer hover:bg-[#2D2F2F] rounded-md p-[4px]" />
+          <Plus className="w-8 cursor-pointer hover:bg-[#2D2F2F] rounded-md p-[4px]" />
         </div>
       </div>
       {children}
--- src/components/textContent.tsx
+++ src/components/textContent.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Geist_Mono } from "next/font/google";
-import { Copy } from "lucide-react";
+import { Copy, QrCode, Link2, X, Trash2 } from "lucide-react";
+
+import { useQRCode } from "next-qrcode";
 
 const geistMono = Geist_Mono({
   weight: ["400", "600"],
@@ -15,13 +17,27 @@ type TextContentPropsType = {
 };
 
 function TextContent({ heading, text }: TextContentPropsType) {
+  const { Image } = useQRCode();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const onClickCopy = () => {
     navigator.clipboard.writeText(text);
   };
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className="pb-5">
-      <h3 className="text-xl font-semibold">{heading}</h3>
+    <div className="pb-5 mt-2 relative">
+      <div className="flex justify-between items-center">
+        <h3 className="text-xl font-semibold">{heading}</h3>
+        <Trash2 className=" h-4 m-1 hover:text-[#c0c1bd] cursor-pointer text-[#949592]" />
+      </div>
       <hr className="mt-1 mb-3 text-[#f5f5f51f]" />
       <div className="bg-[#1F2121] rounded-md">
         <div className="flex justify-between pb-0">
@@ -30,10 +46,17 @@ function TextContent({ heading, text }: TextContentPropsType) {
           >
             text
           </p>
-          <Copy
-            className="h-4 m-1 cursor-pointer text-[#949592]"
-            onClick={onClickCopy}
-          />
+          <div className="flex">
+            <Link2 className="h-4 m-1 hover:text-[#c0c1bd] cursor-pointer text-[#949592]" />
+            <QrCode
+              className="h-4 m-1 hover:text-[#c0c1bd] cursor-pointer text-[#949592]"
+              onClick={openModal}
+            />
+            <Copy
+              className="h-4 m-1 hover:text-[#c0c1bd] cursor-pointer text-[#949592]"
+              onClick={onClickCopy}
+            />
+          </div>
         </div>
         <pre
           className={`${geistMono.className} p-4 whitespace-pre-wrap text-xs text-[#C5C8C6]`}
@@ -41,6 +64,24 @@ function TextContent({ heading, text }: TextContentPropsType) {
           {`${text}`}
         </pre>
       </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-[#000000a9] z-50">
+          <div className="bg-[#1F2121] rounded-lg p-6 relative">
+            <button
+              className="absolute cursor-pointer top-2 right-2 text-[#949592] hover:text-[#c0c1bd]"
+              onClick={closeModal}
+              aria-label="Close"
+            >
+              <X size={20} />
+            </button>
+            <h4 className="text-lg font-semibold mb-4 text-[#949592]">
+              QR Code
+            </h4>
+            <Image text="hello" options={{ width: 200, margin: 2 }} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
