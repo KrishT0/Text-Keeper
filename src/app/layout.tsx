d4249157ce0b1bf34ca6e3ef2cb5bae70a852afd
Krishna Biswal <krshnabiswal619@gmail.com>
2025-09-29 08:00:10 +0530

    added speed insights and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import ToastProvider from "@/components/sonner";
 
@@ -25,6 +27,8 @@ export default function RootLayout({
       >
         {children}
         <ToastProvider />
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
