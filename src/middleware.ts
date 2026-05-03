40c212154f8aa9a381aa5c7a23bbb0b3231007a0
Krishna Biswal <krshnabiswal619@gmail.com>
2026-05-03 14:01:39 +0530

    updated packages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 import { decrypt, deleteSession } from "@/app/utils/session";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const cookie = req.cookies.get("session")?.value;
