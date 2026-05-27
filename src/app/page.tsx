c67b2a782083e61f8eee99830f0659d800264ba5
Krishna Biswal <krshnabiswal619@gmail.com>
2026-05-27 23:57:33 +0530

    updated landig page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,99 @@
-import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
-  const isDeployed = !!process.env.VERCEL_PROJECT_PRODUCTION_URL;
-  const baseUrl = isDeployed
-    ? `${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : "localhost:3000";
+const coreValues = [
+  {
+    title: "VERCEL AI INTELLIGENCE",
+    description:
+      "One-click summarization via Vercel AI Gateway. Extract key variables, logic paths, and actionable points from raw text automatically.",
+  },
+  {
+    title: "ADVANCED MANAGEMENT",
+    description:
+      "Per-snippet tagging for categorization and instant clipboard copy functionality for seamless workflow integration.",
+  },
+  {
+    title: "MOBILE PRESENCE",
+    description:
+      "QR code generation for instant mobile sharing. Access your snippets on any mobile device without manual typing.",
+  },
+  {
+    title: "SECURE ACCESS",
+    description:
+      "JWT-based secure access control and AES-256 encryption. Your data is protected by industry- standard stateless authentication.",
+  },
+];
 
+export default function Home() {
   return (
-    <div className="flex flex-col h-full">
-      <section className="h-1/2 sm:h-2/3 p-10 flex flex-col justify-center w-full">
-        <h1 className="text-5xl sm:text-6xl md:text-7xl font-medium">
-          Store Text Securely
-        </h1>
-        <hr className="w-1/2 sm:w-1/4 mt-7 mb-4" />
-        <Link href="text" className="sm:text-lg w-fit flex items-center gap-2">
-          {baseUrl}/text
-        </Link>
-      </section>
-      <section className="sm:h-1/3 flex gap-16 p-10 w-full">
-        <p className="sm:w-2/3 sm:text-xl">
-          Ideas are powerful when they&apos;re always within reach. With Text
-          Keeper, you can save your text online and access it from any device,
-          anytime. Organize, edit, and stay connected to your notes
-          effortlessly.
+    <div className="h-full px-5">
+      <nav className="text-xs flex justify-between items-center py-4">
+        <p>TEXT KEEPER</p>
+        <Link href="/text">GET STARTED</Link>
+      </nav>
+
+      {/* Hero section */}
+      <section
+        id="hero"
+        className="h-9/12 max-w-195 mx-auto flex flex-col gap-8 justify-center"
+      >
+        <div className="text-4xl sm:text-5xl md:text-5xl font-semibold">
+          <h1>YOUR TEXT,</h1>
+          <h1>EVERYWHERE.</h1>
+        </div>
+        <p className="text-sm sm:w-4/5 md:w-3/5">
+          Minimalist snippet manager with CRUD operations and Cloud PostgreSQL
+          sync. No clutter. Just high-contrast productivity accessible from any
+          device. Minimalist snippet manager with CRUD operations and Cloud
+          PostgreSQL sync. No clutter. Just high-contrast productivity
+          accessible from any device.
         </p>
-        <Image
-          src="/logo.svg"
-          alt="Brand logo"
-          width={50}
-          height={50}
-          loading="lazy"
-          className="hidden sm:block pb-8"
-        />
+        <div className="underline cursor-pointer underline-offset-2 flex gap-8 text-xs">
+          <Link href="/text">GET STARTED</Link>
+          <p>VIEW DEMO</p>
+        </div>
       </section>
+
+      {/* Core value section */}
+      <section
+        id="core-values"
+        className="max-w-195 mx-auto flex flex-col xs:flex-row"
+      >
+        <h2 className="text-xl xs:w-1/3 font-medium">CORE VALUES</h2>
+        <div className="w-[90%] mt-8 xs:mt-0 sm:w-1/2 flex flex-col gap-16 xs:gap-12">
+          {coreValues.map((item) => (
+            <div key={item.title}>
+              <p className="font-medium mb-3">{item.title}</p>
+              <p className="text-sm">{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Bottom section */}
+      <section
+        id="bottom"
+        className="h-2/3 max-w-195 mx-auto flex flex-col gap-5 text-center justify-center"
+      >
+        <div className="text-2xl max-w-[95%] xs:max-w-[70%] sm:max-w-2/3 mx-auto sm:text-2xl md:text-3xl  font-semibold">
+          <h2>EFFICIENCY IS THE ONLY THE ONLY METRICS THAT MATTERS.</h2>
+        </div>
+        <p className="text-sm xs:w-3/5 mx-auto">
+          Minimalist snippet manager with CRUD operations and Cloud PostgreSQL
+          sync. No clutter. Just
+        </p>
+      </section>
+
+      <footer>
+        <hr className="w-full text-[#949592]" />
+        <div className="flex justify-between">
+          <p className="text-xs text-center py-4">
+            &copy; {new Date().getFullYear()} Text Keeper. All rights reserved.
+          </p>
+          <p className="text-xs text-center py-4">
+            <Link href="/privacy">Privacy Policy</Link>
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
